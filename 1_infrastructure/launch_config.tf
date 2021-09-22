6a53afced081030b1cda7d6ce8a25fe1d0d9f2a2
EnriquePernia <Epernia96@gmail.com>
2021-09-23 00:58:58 +0200

    Added launch configuration

--- 1_infrastructure/launch_config.tf
+++ 1_infrastructure/launch_config.tf
@@ -1,9 +1,10 @@
+# Needed por ASG configuration
 resource "aws_launch_configuration" "holded" {
   name          = "holded"
   image_id      = "ami-58d7e821"
   instance_type = "t2.micro"
 
-  security_groups = ["${aws_security_group.backend_http.id}"]
+  security_groups = ["${aws_security_group.ec2_sg.id}"]
 
   user_data = "./run.sh"
 
