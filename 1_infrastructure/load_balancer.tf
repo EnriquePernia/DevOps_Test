a7533f6a50751756a1928eb88f1f603050e2062e
EnriquePernia <Epernia96@gmail.com>
2021-09-23 01:00:09 +0200

    Configured load balancer w lb_target_group

---
+++ 1_infrastructure/load_balancer.tf
@@ -0,0 +1,40 @@
+data "aws_subnet_ids" "holded_subnets" {
+  vpc_id = var.aws_vpc
+}
+
+resource "aws_lb_target_group" "holded_lb_target_group" {
+  name     = "holded-lb-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.aws_vpc
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    interval            = 30
+    protocol            = "HTTP"
+    port                = 80
+    path                = "/"
+  }
+}
+resource "aws_lb" "holded" {
+  name               = "holded"
+  load_balancer_type = "application"
+  subnets = data.aws_subnet_ids.holded_subnets.ids
+  security_groups    = ["${aws_security_group.elb_sg.id}"]
+}
+
+# resource "aws_lb_listener" "holded_listener" {
+#   load_balancer_arn = aws_lb.holded.arn
+#   port              = "80"
+#   protocol          = "HTTP"
+
+#   default_action {
+#     type = "redirect"
+
+#     redirect {
+#       port        = "8080"
+#       protocol    = "HTTP"
+#     }
+#   }
+# }
