d4f0155e854c30636490c9ab119dddac8caf459e
EnriquePernia <Epernia96@gmail.com>
2021-09-23 00:58:28 +0200

    Configured new security group

--- 1_infrastructure/security_group.tf
+++ 1_infrastructure/security_group.tf
@@ -1,6 +1,6 @@
-# Create security group for our instances in ASG(Auto scaling group)
-resource "aws_security_group" "backend_http" {
-  name        = "Allow_holded_http"
+# Create security group for our instances in ASG (Auto scaling group)
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2_sg"
   description = "Allow HTTP inbound traffic"
   vpc_id      = var.aws_vpc
 
@@ -12,6 +12,29 @@ resource "aws_security_group" "backend_http" {
   }
 
   tags = {
-    Name = "allow_http"
+    Name = "ec2_sg"
+  }
+}
+
+# Create security group for ELB (Elastic load balancer)
+resource "aws_security_group" "elb_sg" {
+  name = "Lb_sg"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Lb_sg"
   }
 }
