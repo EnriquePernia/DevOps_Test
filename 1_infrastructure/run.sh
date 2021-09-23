561c5f570bcdc617f21a36472933eb77f7e44ce4
EnriquePernia <Epernia96@gmail.com>
2021-09-23 17:33:09 +0200

    Modified User_data script in order to expose simple API on port 80, added also user data as data file type

--- 1_infrastructure/run.sh
+++ 1_infrastructure/run.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 # Script used for EC2 instance first boot
 
-# Basic packages
+# # Basic packages
+# yum update -y
+# yum install git -y
+
+# # Install and configura docker
+# amazon-linux-extras install docker
+# service docker start
+# usermod -a -G docker ec2-user
+
+# # Install kubectl
+# curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+# chmod +x ./kubectl
+# sudo mv ./kubectl /usr/local/bin/kubectl
+
+# # Install minikube and start it
+# curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+# chmod +x minikube
+# sudo mv minikube /usr/local/bin/
+
+# # Create aliases
+# echo "alias k=kubectl" >> ~/.bashrc
+# source ~/.bashrc
+
 yum update -y
-yum install git -y
-
-# Install and configura docker
-amazon-linux-extras install docker
-service docker start
-usermod -a -G docker ec2-user
-
-# Install kubectl
-curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
-chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin/kubectl
-
-# Install minikube and start it
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-chmod +x minikube
-sudo mv minikube /usr/local/bin/
-
-# Create aliases
-echo "alias k=kubectl" >> ~/.bashrc
-source ~/.bashrc
+yum install -y httpd
+systemctl start httpd
+systemctl enable httpd
+echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
