ed47e412f386495958626fd4a479b69fa633dbae
EnriquePernia <Epernia96@gmail.com>
2021-09-23 17:31:42 +0200

    Added right instance AMI in order to test the infrastructure

--- 1_infrastructure/launch_config.tf
+++ 1_infrastructure/launch_config.tf
@@ -1,15 +1,22 @@
+data "template_file" "web-userdata" {
+  template = "${file("run.sh")}"
+}
+
 # Needed por ASG configuration
 resource "aws_launch_configuration" "holded" {
   name          = "holded"
-  image_id      = "ami-58d7e821"
+  image_id      = "ami-0d1bf5b68307103c2"
   instance_type = "t2.micro"
-
-  security_groups = ["${aws_security_group.ec2_sg.id}"]
-
-  user_data = "./run.sh"
+  key_name = var.aws_key
+  security_groups = ["sg-0663de3e394f4149c"]
+  user_data = data.template_file.web-userdata.rendered
 
   # Ensure we have a launch configuration allways available 
   lifecycle {
     create_before_destroy = true
   }
+}
+
+output "user_data" {
+  value = data.template_file.web-userdata.rendered
 }
