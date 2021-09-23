93eb4e8139fd0a89e64ee9a1b5301b2d0b8464e8
EnriquePernia <Epernia96@gmail.com>
2021-09-23 17:35:58 +0200

    Added autoscaling attachment

--- 1_infrastructure/auto_scaling_group.tf
+++ 1_infrastructure/auto_scaling_group.tf
@@ -1,3 +1,8 @@
+ resource "aws_autoscaling_attachment" "asg_attachment" {
+ autoscaling_group_name = aws_autoscaling_group.holded_asg.name
+ alb_target_group_arn   = aws_lb_target_group.holded_lb_target_group.arn
+}
+
 resource "aws_autoscaling_group" "holded_asg" {
   name                 = "Holded asg"
   launch_configuration = aws_launch_configuration.holded.id
