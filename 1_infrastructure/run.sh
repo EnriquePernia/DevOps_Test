19fd6bf38506c78d286c71bb19a09b7ce30208c3
EnriquePernia <Epernia96@gmail.com>
2021-09-26 10:53:48 +0200

    Removed testing script and added docker and codeDeploy agent installation

--- 1_infrastructure/run.sh
+++ 1_infrastructure/run.sh
@@ -1,31 +1,23 @@
 #!/bin/bash
 # Script used for EC2 instance first boot
 
-# # Basic packages
-# yum update -y
-# yum install git -y
-
-# # Install and configura docker
-# amazon-linux-extras install docker
-# service docker start
-# usermod -a -G docker ec2-user
-
-# # Install kubectl
-# curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
-# chmod +x ./kubectl
-# sudo mv ./kubectl /usr/local/bin/kubectl
-
-# # Install minikube and start it
-# curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-# chmod +x minikube
-# sudo mv minikube /usr/local/bin/
-
-# # Create aliases
-# echo "alias k=kubectl" >> ~/.bashrc
-# source ~/.bashrc
-
+# Basic packages
 yum update -y
-yum install -y httpd
-systemctl start httpd
-systemctl enable httpd
-echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
+yum install git -y
+
+# Install and configura docker
+amazon-linux-extras install docker
+service docker start
+usermod -a -G docker ec2-user
+
+# Install codeDeploy agent
+sudo yum update 
+sudo yum install -y ruby
+sudo yum install wget
+wget https://aws-codedeploy-ap-southeast-1.s3.ap-southeast-1.amazonaws.com/latest/install
+chmod +x ./install
+sudo ./install auto
+sudo service codedeploy-agent start
+
+# Cole repository
+git clone https://github.com/EnriquePernia/DevOps_Test.git
