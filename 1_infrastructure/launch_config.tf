5efd9ee1a0a625530623f443aa14b24f308c8f11
EnriquePernia <Epernia96@gmail.com>
2021-09-26 10:53:09 +0200

    Added IAM role in order to use codeDeploy

--- 1_infrastructure/launch_config.tf
+++ 1_infrastructure/launch_config.tf
@@ -10,10 +10,14 @@ resource "aws_launch_configuration" "holded" {
   key_name = var.aws_key
   security_groups = [aws_security_group.ec2_sg.id]
   user_data = data.template_file.web-userdata.rendered
-
+  iam_instance_profile = aws_iam_instance_profile.codeDeploy_profile
   # Ensure we have a launch configuration allways available 
   lifecycle {
     create_before_destroy = true
+  }
+  tag {
+    key                 = "Project"
+    value               = "Holded"
   }
 }
 
