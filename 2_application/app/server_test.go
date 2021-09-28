e164296df4b8f9dfddb1619f926897469f35c662
Enrique Pernía Garcia <36536477+EnriquePernia@users.noreply.github.com>
2021-09-28 16:19:49 +0200

    Update server_test.go

--- 2_application/app/server_test.go
+++ 2_application/app/server_test.go
@@ -59,7 +59,7 @@ func TestHealthCheckHandler(t *testing.T) {
     }
 
     // Check the response body is what we expect.
-    expected := `{"alie": true}`
+    expected := `{"alive": true}`
     if rr.Body.String() != expected {
         t.Errorf("handler returned unexpected body: got %v want %v",
             rr.Body.String(), expected)
